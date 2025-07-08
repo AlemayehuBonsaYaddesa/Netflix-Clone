9dff4f41cbf1fcd536467933e89090cf5afb2f3f
AlemayehuBonsaYaddesa <150921885+AlemayehuBonsaYaddesa@users.noreply.github.com>
2025-07-08 09:59:21 +0300

    Footer and Header done

---
+++ netflix-clone/src/Components/Header/Header.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Logo from "../../assets/images/netflix.png";
+import "./header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownCircleIcon from "@mui/icons-material/ArrowDropDownCircle";
+const Header = () => {
+  return (
+    <div className="header_outer_container">
+      <div className="header_container">
+        <div className="header_left">
+          <ul>
+            <li>
+              <img src={Logo} alt="Netflix" />
+            </li>
+            <li>Home</li>
+            <li>TVShows</li>
+            <li>Movies</li>
+            <li>Latest</li>
+            <li>MyList</li>
+            <li>Browse by Language</li>
+          </ul>
+        </div>
+        <div className="header_right">
+          <ul>
+            <li>
+              <SearchIcon />
+            </li>
+            <li>
+              <NotificationsIcon />
+            </li>
+            <li>
+              <AccountBoxIcon />
+            </li>
+            <li>
+              <ArrowDropDownCircleIcon />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+// function Header() {
+//   return (
+//     <div>
+//       Header
+//     </div>
+//   )
+// }
+
+export default Header;
